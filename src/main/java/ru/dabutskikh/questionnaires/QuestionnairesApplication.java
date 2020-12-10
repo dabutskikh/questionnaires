f3fb39dc2e7a11fa266a6a51168b55ed021c00e7
Denis Butskikh <dabutskikh@gmail.com>
2020-12-10 17:26:10 +0400

    Initial commit

---
+++ src/main/java/ru/dabutskikh/questionnaires/QuestionnairesApplication.java
@@ -0,0 +1,13 @@
+package ru.dabutskikh.questionnaires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnairesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dabutskikh/questionnaires/QuestionnairesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dabutskikh.questionnaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
