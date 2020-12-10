b906dc218bd6bc51ce784ae91581143688a2a1d6
Denis Butskikh <dabutskikh@gmail.com>
2020-12-10 18:17:03 +0400

    Add class Question

---
+++ src/main/java/ru/dabutskikh/questionnaires/model/Question.java
@@ -0,0 +1,39 @@
+package ru.dabutskikh.questionnaires.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "question")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Question() {
+    }
+
+    public Question(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
