108633d3c5577ba4c43448d230c1d0f7533102e1
Denis Butskikh <dabutskikh@gmail.com>
2020-12-11 20:48:13 +0400

    Fix interface UserService

--- src/main/java/ru/dabutskikh/questionnaires/service/UserService.java
+++ src/main/java/ru/dabutskikh/questionnaires/service/UserService.java
@@ -2,9 +2,11 @@ package ru.dabutskikh.questionnaires.service;
 
 import ru.dabutskikh.questionnaires.model.User;
 
+import java.util.Optional;
+
 public interface UserService {
 
     void save(User user);
 
-    User findByLogin(String login);
+    Optional<User> findByLogin(String login);
 }
