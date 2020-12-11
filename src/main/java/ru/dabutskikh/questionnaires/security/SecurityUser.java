12c8f0aefe08eef0123c65e60551c4fa93b5bb80
Denis Butskikh <dabutskikh@gmail.com>
2020-12-11 13:26:16 +0400

    Add class SecurityUser

---
+++ src/main/java/ru/dabutskikh/questionnaires/security/SecurityUser.java
@@ -0,0 +1,69 @@
+package ru.dabutskikh.questionnaires.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.dabutskikh.questionnaires.model.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+
+    public static UserDetails fromUser(User user) {
+        return new org.springframework.security.core.userdetails.User(
+                user.getLogin(),
+                user.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                user.getRole().getAuthorities()
+        );
+    }
+
+    private final String username;
+    private final String password;
+    private final List<SimpleGrantedAuthority> authorities;
+
+    public SecurityUser(String username, String password, List<SimpleGrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
