62b7c8ee022ce2e4307e20b3c03250f6ed738fb6
Denis Butskikh <dabutskikh@gmail.com>
2020-12-11 13:31:43 +0400

    Add class UserDetailsServiceImpl

---
+++ src/main/java/ru/dabutskikh/questionnaires/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.dabutskikh.questionnaires.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.dabutskikh.questionnaires.model.User;
+import ru.dabutskikh.questionnaires.repository.UserRepository;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(login).orElseThrow(() ->
+                new UsernameNotFoundException("User doesn't exists"));
+        return SecurityUser.fromUser(user);
+    }
+}
