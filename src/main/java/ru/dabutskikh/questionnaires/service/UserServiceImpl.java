e35d61183f7d788696a8ad385e4cd0f0448555f3
Denis Butskikh <dabutskikh@gmail.com>
2020-12-11 20:49:38 +0400

    Add class UserServiceImpl

---
+++ src/main/java/ru/dabutskikh/questionnaires/service/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package ru.dabutskikh.questionnaires.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.dabutskikh.questionnaires.model.Role;
+import ru.dabutskikh.questionnaires.model.User;
+import ru.dabutskikh.questionnaires.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
+}
