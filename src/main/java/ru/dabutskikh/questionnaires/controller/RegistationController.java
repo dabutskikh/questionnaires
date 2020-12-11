289f5da35cc6271cf946bad16ec168ecf3ff1c7b
Denis Butskikh <dabutskikh@gmail.com>
2020-12-11 15:09:57 +0400

    Add class RegistrationController

---
+++ src/main/java/ru/dabutskikh/questionnaires/controller/RegistationController.java
@@ -0,0 +1,36 @@
+package ru.dabutskikh.questionnaires.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.dabutskikh.questionnaires.model.User;
+import ru.dabutskikh.questionnaires.repository.UserRepository;
+
+@Controller
+@RequestMapping("/registation")
+public class RegistationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping
+    public String getRegistationPage(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @PostMapping
+    public String addUser(@ModelAttribute("userForm") User userForm) {
+        if (userRepository.findByLogin(userForm.getLogin()).isPresent()) {
+            userRepository.save(userForm);
+            return "registation";
+        } else {
+            return "redirect:/";
+        }
+    }
+}
