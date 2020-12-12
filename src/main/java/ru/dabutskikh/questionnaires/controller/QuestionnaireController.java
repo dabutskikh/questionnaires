371d72c9bb7ba10226b51cdbec8fae8241cb6c4d
Denis Butskikh <dabutskikh@gmail.com>
2020-12-12 13:35:36 +0400

    Implement method getCreatingQuestionnairePage in controller

--- src/main/java/ru/dabutskikh/questionnaires/controller/QuestionnaireController.java
+++ src/main/java/ru/dabutskikh/questionnaires/controller/QuestionnaireController.java
@@ -1,14 +1,27 @@
 package ru.dabutskikh.questionnaires.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.dabutskikh.questionnaires.model.Questionnaire;
+import ru.dabutskikh.questionnaires.service.QuestionnaireService;
+import ru.dabutskikh.questionnaires.service.UserService;
 
 @Controller
 @RequestMapping("/questionnaires")
 public class QuestionnaireController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    QuestionnaireService questionnaireService;
 
     @GetMapping
     public String getAllQuestionnaires() {
@@ -21,8 +34,11 @@ public class QuestionnaireController {
     }
 
     @GetMapping("/new")
-    public String getCreatingQuestionnairePage() {
-        return null;
+    public String getCreatingQuestionnairePage(Model model,
+                                               @AuthenticationPrincipal UserDetails currentUser) {
+        model.addAttribute("author", userService.findByLogin(currentUser.getUsername()));
+        model.addAttribute("questionnaireForm", new Questionnaire());
+        return "new_questionnaire";
     }
 
     @GetMapping("/{id}")
