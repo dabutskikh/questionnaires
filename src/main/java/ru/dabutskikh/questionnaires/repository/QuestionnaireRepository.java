3fe7ba899b7a8e6d97615b767557080ed631e189
Denis Butskikh <dabutskikh@gmail.com>
2020-12-13 12:31:29 +0400

    Minor fix in repositories

--- src/main/java/ru/dabutskikh/questionnaires/repository/QuestionnaireRepository.java
+++ src/main/java/ru/dabutskikh/questionnaires/repository/QuestionnaireRepository.java
@@ -3,14 +3,5 @@ package ru.dabutskikh.questionnaires.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.dabutskikh.questionnaires.model.Questionnaire;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long> {
-
-    @Override
-    List<Questionnaire> findAll();
-
-    @Override
-    Optional<Questionnaire> findById(Long aLong);
 }
