61cd83c7d2c74e98fc003aa67428a888cffb40d7
Denis Butskikh <dabutskikh@gmail.com>
2020-12-13 20:31:10 +0400

    Fix AnswerRepository

--- src/main/java/ru/dabutskikh/questionnaires/repository/AnswerRepository.java
+++ src/main/java/ru/dabutskikh/questionnaires/repository/AnswerRepository.java
@@ -1,7 +1,18 @@
 package ru.dabutskikh.questionnaires.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.dabutskikh.questionnaires.model.Answer;
 
+import java.util.List;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    List<Answer> findByQuestionId(Long questionnaireId);
+
+    @Modifying
+    @Query("delete from Answer a where a.id=:id")
+    void deleteById(@Param("id") Long id);
 }
