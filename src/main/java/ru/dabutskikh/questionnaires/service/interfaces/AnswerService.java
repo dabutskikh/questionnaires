dd6ed4c110d80b70563f60793cace5e9fac37269
Denis Butskikh <dabutskikh@gmail.com>
2020-12-13 20:32:55 +0400

    Fix AnswerService

--- src/main/java/ru/dabutskikh/questionnaires/service/interfaces/AnswerService.java
+++ src/main/java/ru/dabutskikh/questionnaires/service/interfaces/AnswerService.java
@@ -1,4 +1,18 @@
 package ru.dabutskikh.questionnaires.service.interfaces;
 
+import ru.dabutskikh.questionnaires.model.Answer;
+
+import java.util.List;
+
 public interface AnswerService {
+
+    Answer findById(Long id);
+
+    void update(Long id, Answer answer);
+
+    void delete(Long id);
+
+    void save(Answer answer);
+
+    List<Answer> getAllAnswersByQuestionId(Long questionId);
 }
